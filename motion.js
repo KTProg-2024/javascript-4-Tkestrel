96f7441c6b6e4ecfe5972101904fab07a7b39a90
Toby Hansen <>
2021-05-28 14:18:51 +0000

    moveLeft and moveUp buttons need to be fixed

--- motion.js
+++ motion.js
@@ -1,8 +1,57 @@
 var posTop = 0;
 var posLeft = 0;
+var posRight = 0;
+var posBottom = 0;
 
+function moveDown() {
+  var elem = document.getElementById("animate");
+  var id = setInterval(frame, 5);
+  function frame() {
+	  posBottom++;
+	  elem.style.top = posBottom + "px";
+	  if (posBottom == 350)
+		  clearInterval(id);
+  }
+}
+
+function moveUp() {
+  var elem = document.getElementById("animate");  
+  var id = setInterval(frame, 5);
+  function frame() {
+	  posTop++;
+	  elem.style.bottom = posTop + "px";
+	  if (posTop == 350)
+		  clearInterval(id);
+  }
+}
+
+function moveLeft() {
+  var elem = document.getElementById("animate");  
+  var id = setInterval(frame, 5);
+  function frame() {
+	  posLeft++;
+	  elem.style.right = posLeft + "px";
+	  if (posLeft == 350)
+		  clearInterval(id);
+  }
+}
+
+function moveRight() {
+  var elem = document.getElementById("animate");  
+  var id = setInterval(frame, 5);
+  function frame() {
+	  posRight++;
+	  elem.style.left = posRight + "px";
+	  if (posRight == 350)
+		  clearInterval(id);
+  }
+}
+
+/*
 function myMove() {
-  var elem = document.getElementById("animate");    
+  var elem = document.getElementById("animate");   
+  var posTop = 0;
+  var posLeft = 0;
   var id = setInterval(frame, 5);
   function frame() {
 	  posTop++;
@@ -13,3 +62,4 @@ function myMove() {
 		  clearInterval(id);
   }
 }
+*/
